c67665dce0d9e0f803fcf265d2be15a878901658
danielleilyasov <danielle.ilyas@gmail.com>
2022-08-24 01:32:34 +0300

    add coords on screen, fix scc all app

--- src/app/cmps/show-coords/show-coords.component.ts
+++ src/app/cmps/show-coords/show-coords.component.ts
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShowCoordsComponent implements OnInit {
 
+  x: number = 0
+  y: number = 0
+
   constructor() { }
 
   ngOnInit(): void {
+      window.addEventListener('mousemove', this.onMouseMove)
+  }
+
+  onMouseMove = ({ x, y }: MouseEvent) => {
+      this.x = x
+      this.y = y
   }
 
 }
