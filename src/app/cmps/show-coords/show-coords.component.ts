8bed19eee99d70fd8f6429d2b9f44b09036a6357
danielleilyasov <danielle.ilyas@gmail.com>
2022-08-24 01:02:25 +0300

    add new cmp show mouse coords on screen

---
+++ src/app/cmps/show-coords/show-coords.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-show-coords',
+  templateUrl: './show-coords.component.html',
+  styleUrls: ['./show-coords.component.scss']
+})
+export class ShowCoordsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
